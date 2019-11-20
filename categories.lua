22669d06149e6eeb43243d96817aa31c9ec56478
Striiiider <57972014+Striiiider@users.noreply.github.com>
2019-11-19 19:57:18 -0600

    Create categories.lua

---
+++ categories.lua
@@ -0,0 +1,116 @@
+DarkRP.createCategory{
+    name = "Stormtroopers",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 255, 255, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 1
+}
+
+DarkRP.createCategory{
+    name = "181st Armor Division",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 255, 255, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 2
+}
+
+DarkRP.createCategory{
+    name = "212th Attack Battalion",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 152, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 3
+}
+
+DarkRP.createCategory{
+    name = "442nd Seige Battalion",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(0, 214, 5, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 4
+}
+
+DarkRP.createCategory{
+    name = "501st Legion - Vader's Fist",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(17, 0, 255, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 5
+}
+
+DarkRP.createCategory{
+    name = "Death Troopers",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 6
+}
+
+DarkRP.createCategory{
+    name = "Imperial Commandos",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 7
+}
+
+DarkRP.createCategory{
+    name = "Imperial Guardsmen",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 8
+}
+
+DarkRP.createCategory{
+    name = "ISB",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 255, 255, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 9
+}
+
+DarkRP.createCategory{
+    name = "Jet Troopers",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(135, 127, 127, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 10
+}
+
+DarkRP.createCategory{
+    name = "Naval",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 11
+}
+
+DarkRP.createCategory{
+    name = "Scout Troopers",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(255, 255, 255, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 12
+}
+
+DarkRP.createCategory{
+    name = "Starfighter Corps",
+    categorises = "jobs",
+    startExpanded = false,
+    color = Color(0, 0, 0, 255),
+    canSee = function(ply) return true end,
+    sortOrder = 13
+}
